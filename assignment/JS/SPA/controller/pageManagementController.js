896e7d4f6d3777068fad7671ba37770e708e2941
lakmal05 <lakmaljayawardhana59@gmail.com>
2022-10-12 19:44:59 +0530

    Item methord invokig

--- assignment/JS/SPA/controller/pageManagementController.js
+++ assignment/JS/SPA/controller/pageManagementController.js
@@ -1,59 +1,44 @@
+
 $(document).ready(function () {
     $('#dashboard').show();
-    $('#customerSection1').hide();
-    $('#customerSection2').hide();
-    $('#itemSection1').hide();
-    $('#itemSection2').hide();
-    $('#orderSection1').hide();
-    $('#orderSection2').hide();
+    $('#customerSection').hide();
+    $('#itemSection').hide();
+    $('#orderSection').hide();
 });
 
-// Start When home click other section hide
+
+//Start When home click other section hide
 $('#home').click(function () {
     $('#dashboard').css('display', 'block')
-    $('#customerSection1').css('display', 'none');
-    $('#customerSection2').css('display', 'none');
-    $('#itemSection1').css('display', 'none');
-    $('#itemSection2').css('display', 'none');
-    $('#orderSection1').css('display', 'none');
-    $('#orderSection2').css('display', 'none');
+    $('#customerSection').css('display', 'none');
+    $('#itemSection').css('display', 'none');
+    $('#orderSection').css('display', 'none');
 });
 // End When home click other section hide
 
 // Start When customer click other section hide
 $('#customer').click(function () {
     $('#dashboard').css('display', 'none')
-    $('#customerSection1').css('display', 'block');
-    $('#customerSection2').css('display', 'block');
-    $('#itemSection1').css('display', 'none');
-    $('#itemSection2').css('display', 'none');
-    $('#orderSection1').css('display', 'none');
-    $('#orderSection2').css('display', 'none');
+    $('#customerSection').css('display', 'block');
+    $('#itemSection').css('display', 'none');
+    $('#orderSection').css('display', 'none');
 });
 // End When customer click other section hide
 
 // Start When item click other section hide
 $('#item').click(function () {
     $('#dashboard').css('display', 'none')
-    $('#customerSection1').css('display', 'none');
-    $('#customerSection2').css('display', 'none');
-    $('#itemSection1').css('display', 'block');
-    $('#itemSection2').css('display', 'block');
-    $('#orderSection1').css('display', 'none');
-    $('#orderSection2').css('display', 'none');
+    $('#customerSection').css('display', 'none');
+    $('#itemSection').css('display', 'block');
+    $('#orderSection').css('display', 'none');
 });
 // End When item click other section hide
 
 // Start When order click other section hide
 $('#order').click(function () {
     $('#dashboard').css('display', 'none')
-    $('#customerSection1').css('display', 'none');
-    $('#customerSection2').css('display', 'none');
-    $('#itemSection1').css('display', 'none');
-    $('#itemSection2').css('display', 'none');
-    // $('#orderSection1').css('display', 'block');
-    // $('#orderSection2').css('display', 'block');
-    $('#orderSection1').show();
-    $('#orderSection2').show();
+    $('#customerSection').css('display', 'none');
+    $('#itemSection').css('display', 'none');
+    $('#orderSection').show();
+
 });
-// End When order click other section hide
