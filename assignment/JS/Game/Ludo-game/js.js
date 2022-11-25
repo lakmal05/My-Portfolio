183ca06a7e410b6cbf14f25cdb6e2e7b32a19083
lakmal05 <lakmaljayawardhana59@gmail.com>
2022-11-25 18:34:12 +0530

    Push Step Right,Left,up,Down

--- assignment/JS/Game/Ludo-game/js.js
+++ assignment/JS/Game/Ludo-game/js.js
@@ -21,8 +21,6 @@ var pawnOut = {red:0,blue:0,green:0,yellow:0}
 
 
 
-
-
 function ResetPawn(victim) {
     onboard[victim] = 0;
     positions[victim] = 0;
@@ -171,10 +169,38 @@ function randomMove(Color, paw) {
 
 
 
-
-var stepsRed = [];
-
-
+var positions = {
+    redpawn1: 0, redpawn2: 0, redpawn3: 0, redpawn4: 0,
+    bluepawn1: 0, bluepawn2: 0, bluepawn3: 0, bluepawn4: 0,
+    greenpawn1: 0, greenpawn2: 0, greenpawn3: 0, greenpawn4: 0,
+    yellowpawn1: 0, yellowpawn2: 0, yellowpawn3: 0, yellowpawn4: 0
+};
+var onboard = {
+    redpawn1: 0, redpawn2: 0, redpawn3: 0, redpawn4: 0,
+    bluepawn1: 0, bluepawn2: 0, bluepawn3: 0, bluepawn4: 0,
+    greenpawn1: 0, greenpawn2: 0, greenpawn3: 0, greenpawn4: 0,
+    yellowpawn1: 0, yellowpawn2: 0, yellowpawn3: 0, yellowpawn4: 0
+};
+function DontHaveOtherFree() {
+    var text = document.getElementById('player');
+    for (var i = 1; i <=4; i++) {
+        if (onboard[text.innerText + "pawn" + i] == 1 || positions[text.innerText + "pawn" + i]+num>=44) return false;
+    }
+    return true;
+}
+function CheckForWinner() {
+    if (pawnOut[currcolor] == 4) {
+        var dice = document.getElementById("dice");
+        var player = document.getElementById("player");
+        var uselesstext1 = document.getElementById("uselesstext1");
+        var uselesstext2 = document.getElementById("uselesstext2");
+        dice.innerText = "";
+        dice.style.visibility = "hidden";
+        uselesstext1.innerText = "";
+        uselesstext2.innerText = "";
+        player.innerText = "The Winner is the "+currcolor+" player";
+    }
+}
 function stepDown() {
     var doc = document.getElementById(currcolor + "pawn"+NumOfPaw);
     var curr = Number(doc.style.top.replace(/[a-z]/g, ''));
@@ -199,12 +225,14 @@ function stepRight() {
     doc.style.left = (curr + step) + 'px';
     currPos++;
 }
-
+var stepsRed = [];
+var stepsYellow = [];
+var stepsBlue =[];
+var stepsGreen =[];
 function pushSteps(value, steps, count) {
     for (i = 0; i < count; i++) steps.push(value);
 }
-
-/*Red Respond path*/
+//Red pawns path
 pushSteps(stepDown,stepsRed,4);
 pushSteps(stepRight, stepsRed,4);
 pushSteps(stepDown, stepsRed,2);
@@ -218,3 +246,48 @@ pushSteps(stepRight, stepsRed,4);
 pushSteps(stepUp, stepsRed,4);
 pushSteps(stepRight, stepsRed,1);
 pushSteps(stepDown, stepsRed,5);
+//Yellow pawns path
+
+pushSteps(stepUp, stepsYellow,4);
+pushSteps(stepLeft, stepsYellow,4);
+pushSteps(stepUp, stepsYellow,2);
+pushSteps(stepRight, stepsYellow,4);
+pushSteps(stepUp, stepsYellow,4);
+pushSteps(stepRight, stepsYellow,2);
+pushSteps(stepDown, stepsYellow,4);
+pushSteps(stepRight, stepsYellow,4);
+pushSteps(stepDown, stepsYellow,2);
+pushSteps(stepLeft, stepsYellow,4);
+pushSteps(stepDown, stepsYellow,4);
+pushSteps(stepLeft, stepsYellow,1);
+pushSteps(stepUp, stepsYellow,5);
+
+//Blue pawns path
+pushSteps(stepLeft, stepsBlue,4);
+pushSteps(stepDown, stepsBlue,4);
+pushSteps(stepLeft, stepsBlue,2);
+pushSteps(stepUp, stepsBlue,4,2);
+pushSteps(stepLeft, stepsBlue,4);
+pushSteps(stepUp, stepsBlue,2);
+pushSteps(stepRight, stepsBlue,4);
+pushSteps(stepUp, stepsBlue,4);
+pushSteps(stepRight, stepsBlue,2);
+pushSteps(stepDown, stepsBlue,4);
+pushSteps(stepRight, stepsBlue,4);
+pushSteps(stepDown, stepsBlue,1);
+pushSteps(stepLeft, stepsBlue,5);
+
+//Green pawns path
+pushSteps(stepRight, stepsGreen,4);
+pushSteps(stepUp, stepsGreen,4);
+pushSteps(stepRight, stepsGreen,2);
+pushSteps(stepDown, stepsGreen,4);
+pushSteps(stepRight, stepsGreen,4);
+pushSteps(stepDown, stepsGreen,2);
+pushSteps(stepLeft, stepsGreen,4);
+pushSteps(stepDown, stepsGreen,4);
+pushSteps(stepLeft, stepsGreen,2);
+pushSteps(stepUp, stepsGreen,4);
+pushSteps(stepLeft, stepsGreen,4);
+pushSteps(stepUp, stepsGreen,1);
+pushSteps(stepRight, stepsGreen, 5);
