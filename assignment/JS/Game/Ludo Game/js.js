e2e215dcc5918e7b46c902263ebfa8b14bcddbeb
lakmal05 <lakmaljayawardhana59@gmail.com>
2022-11-25 09:51:46 +0530

    dic jpg & and random methord

---
+++ assignment/JS/Game/Ludo Game/js.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+/*Dick rowlling and aded random num*/
+function randomNum() {
+    if (!clicked) {
+        num = Math.floor((Math.random() * 6) + 1);;
+        var dice = document.getElementById('dice');
+        dice.style.backgroundImage = "url(asests/" + num + ".jpg)";
+        clicked = true;
+    }
+    if (num != 6&&DontHaveOtherFree()) {
+        var bad = document.getElementById('badtext');
+        bad.innerText = "Unfortunatlly you stuck";
+        window.setTimeout(changePlayer, 1000);
+        clicked = false;
+    }
+}
