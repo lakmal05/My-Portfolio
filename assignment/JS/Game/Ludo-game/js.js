0f35de4a2dee1fc0e711f162f5a53f711f439ccc
lakmal05 <lakmaljayawardhana59@gmail.com>
2022-11-25 18:36:45 +0530

    css link js positions set

--- assignment/JS/Game/Ludo-game/js.js
+++ assignment/JS/Game/Ludo-game/js.js
@@ -1,5 +1,4 @@
 
-
 /*dic num*/
 var num = 0;
 /*Dic Clikced*/
@@ -15,7 +14,35 @@ var currpawn = "";
 var allcolor = ["red", "blue", "green", "yellow"];
 var pawnOut = {red:0,blue:0,green:0,yellow:0}
 
-
+function HaveHover() {
+    var count = 0;
+    var toKill = "";
+    for (var i = 0; i < allcolor.length; i++) {
+        for (var n = 1; n <= 4; n++) {
+            var firstPawn = document.getElementById(allcolor[i] + "pawn" + n);
+            var secondPawn=document.getElementById(currpawn);
+            if (firstPawn.style.top==secondPawn.style.top&&firstPawn.style.left==secondPawn.style.left&&currcolor!=allcolor[i]&&currPos+num<44) {
+                count++;
+                toKill = allcolor[i] + "pawn" + n;
+                return toKill;
+            }
+        }
+    }
+    return false;
+}
+function Stuck() {
+    var text = document.getElementById('player');
+    if (onboard[currpawn] == 0||currPos+num>44) {
+        if (DontHaveOtherFree()||currPos+num>44) {
+            var badtext = document.getElementById('badtext');
+            badtext.innerText = "Unfortunatlly you stuck";
+            clicked = false;
+            var dice = document.getElementById('dice');
+            dice.style.backgroundImage = "url(Images/dice.gif)";
+            window.setTimeout(changePlayer, 1000);
+        }
+    }
+}
 
 
 
@@ -159,7 +186,7 @@ function randomMove(Color, paw) {
                     num = 0;
                     clicked = false;
                     var dice = document.getElementById('dice');
-                    dice.style.backgroundImage = "url(Images/dice.gif)";
+                    dice.style.backgroundImage = "url(asests/dice.gif)";
                 }
                 else Stuck();
             }
@@ -181,6 +208,7 @@ var onboard = {
     greenpawn1: 0, greenpawn2: 0, greenpawn3: 0, greenpawn4: 0,
     yellowpawn1: 0, yellowpawn2: 0, yellowpawn3: 0, yellowpawn4: 0
 };
+
 function DontHaveOtherFree() {
     var text = document.getElementById('player');
     for (var i = 1; i <=4; i++) {
