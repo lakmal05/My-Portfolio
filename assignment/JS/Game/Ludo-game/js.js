e46064d1108bddbe82c9002633c4edba5f53061c
lakmal05 <lakmaljayawardhana59@gmail.com>
2022-11-25 10:26:19 +0530

    icon selected using js

--- assignment/JS/Game/Ludo-game/js.js
+++ assignment/JS/Game/Ludo-game/js.js
@@ -5,7 +5,15 @@ var num = 0;
 /*Dic Clikced*/
 var clicked = false;
 
-
+var currPos = 0;
+var step = 49.5;
+var currcolor = "";
+var NumOfPaw = "";
+// var num = 0;
+// var clicked = false;
+var currpawn = "";
+var allcolor = ["red", "blue", "green", "yellow"];
+var pawnOut = {red:0,blue:0,green:0,yellow:0}
 
 
 /*Dick rowlling and aded random num*/
@@ -21,5 +29,95 @@ function randomNum() {
         bad.innerText = "Unfortunatlly you stuck";
         window.setTimeout(changePlayer, 1000);
         clicked = false;
+    }
+}
+
+
+
+
+/*Bord icon moving*/
+function randomMove(Color, paw) {
+    var text = document.getElementById('player');
+    NumOfPaw = paw;
+    currcolor = Color;
+    currpawn = currcolor + "pawn" + NumOfPaw;
+    currPos = positions[currpawn];
+    if (num + currPos > 44) {
+        Stuck();
+    }
+    else {
+        if (clicked) {
+            var position = currPos;
+            if (text.innerText == currcolor) {
+                if (onboard[currpawn] === 1 || num === 6) {
+                    if (onboard[currpawn] === 0) {
+                        var doc = document.getElementById(currpawn);
+                        var curr = Number(doc.style.left.replace(/[a-z]/g, ''));
+                        switch (Color) {
+                            case "red":
+                                doc.style.left = 318 + 'px';
+                                doc.style.top = 28 + "px";
+                                break;
+
+                            case "yellow":
+                                doc.style.left = 219 + 'px';
+                                doc.style.top = 523 + "px";
+                                break;
+
+                            case "blue":
+                                doc.style.left = 516 + 'px';
+                                doc.style.top = 325 + "px";
+                                break;
+
+                            case "green":
+                                doc.style.left = 21 + 'px';
+                                doc.style.top = 226 + "px";
+                                break;
+                        }
+                        onboard[currpawn] = 1;
+                    }
+                    else {
+                        switch (Color) {
+                            case "red":
+                                for (i = currPos; i < position + num; i++) {
+                                    stepsRed[i]();
+                                }
+                                break;
+
+                            case "yellow":
+                                for (i = currPos; i < position + num; i++) {
+                                    stepsYellow[i]();
+                                }
+                                break;
+
+                            case "blue":
+                                for (i = currPos; i < position + num; i++) {
+                                    stepsBlue[i]();
+                                }
+                                break;
+
+                            case "green":
+                                for (i = currPos; i < position + num; i++) {
+                                    stepsGreen[i]();
+                                }
+                                break;
+                        }
+                        positions[currpawn] = currPos;
+                        var victim = HaveHover();
+                        if (victim != false) {
+                            ResetPawn(victim);
+                        }
+                        if (currPos == 44) { pawnOut[currcolor]++; onboard[currpawn] = 0; positions[currpawn] = 0; document.getElementById(currpawn).style.visibility = "hidden"; };
+                        CheckForWinner();
+                        changePlayer();
+                    }
+                    num = 0;
+                    clicked = false;
+                    var dice = document.getElementById('dice');
+                    dice.style.backgroundImage = "url(Images/dice.gif)";
+                }
+                else Stuck();
+            }
+        }
     }
 }
